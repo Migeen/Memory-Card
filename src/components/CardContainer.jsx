d1acb62db530c252d097306a1fbe59db59c64705
Migeen Lamichhane Magar <migeen999@gmail.com>
2024-12-18 21:52:25 +0545

    first commit for todau

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import GetImages from './GetImages';
 
 const gameCards =  async (len) => {
@@ -13,7 +13,6 @@ const gameCards =  async (len) => {
 } 
 
 const Card = () => {
-
     return (
         <div className='w-44 h-56 border border-black rounded p-1 cursor-pointer'>
             
@@ -22,15 +21,72 @@ const Card = () => {
 }
 
 const CardContainer = () => {
+    
+    const [score, setScore] = useState(0);
+    const [card, setCard] = useState([]);
+    const [highest, setHighest] = useState(0);
+    const [load, setLoad] = useState(true);
+    
+    useEffect(()=> {
+        async function loadCards() {
+            const cardData = await gameCards(9);
+            setCard(cardData);
+            setLoad(false);
+        }
+
+    },[]);
+
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i >= 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+      }
+    
+      function clickPic(pic, event) {
+        event.currentTarget.blur();
+        if (pic[1] == 0) {
+          setScore(score + 1);
+          if (score >= highest) {
+            setHighest(score + 1);
+          }
+          setCard(
+            card.map((car) => {
+              if (car == pic) {
+                //let it be read
+                car[1] = 1;
+                return car;
+              } else return car;
+            })
+          );
+          let newCard = card;
+          shuffleArray(newCard);
+          setCard(newCard);
+        } else {
+          setScore(0);
+          setCard(
+            card.map((car) => {
+              return [car[0], 0];
+            })
+          );
+        }
+      }
+    if(load) return <h1>Loading</h1>
+    else{    
     return (
         <div className='bg-gray-700 w-ful h-4/5 p-7 flex justify-center'>
             <div className='bg-white w-4/5 h-full flex flex-wrap p-20 gap-14 '>
-                <Card/>
+                {card.map((pic)=> (
+                    <Card/>
                     
-                <Card/>          
+                ))}          
             </div>
         </div>
     )
+}   
+
 }
 
 export default CardContainer
